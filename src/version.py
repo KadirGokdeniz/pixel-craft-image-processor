47427e4cf50bc5a4ddf2c09a4b4df4eb1331c754
Kadir Gokdeniz <kadirqokdeniz@hotmail.com>
2025-03-22 21:05:54 +0300

    added version.py

---
+++ src/version.py
@@ -0,0 +1,9 @@
+"""
+PixelCraft version information.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Kadir Gökdeniz"
+__author_email__ = "kadirqokdeniz@hotmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025 Kadir Gökdeniz"
